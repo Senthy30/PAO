3785c82e9352533c5e3ee4248f0d1d8638035f49
Senth <denis-florin.cringanu@s.unibuc.ro>
2023-02-27 17:24:19 +0200

    update

---
+++ src/main/java/laborator1/lab1.java
@@ -0,0 +1,19 @@
+package main.java.laborator1;
+
+public class lab1 {
+
+    private int numberLab;
+
+    public int getNumberLab() {
+        return numberLab;
+    }
+
+    public void setNumberLab(int numberLab) {
+        this.numberLab = numberLab;
+    }
+
+    public void printLine(){
+        System.out.println("salut numele meu e denis");
+    }
+
+}
---
+++ src/main/java/test/tclass.java
@@ -0,0 +1,19 @@
+package main.java.test;
+
+import main.java.laborator1.lab1;
+
+public class tclass {
+
+    private  main.java.laborator1.lab1 a = new lab1();
+
+    public static void main(String[] args) {
+
+        main.java.laborator1.lab1 b = new lab1();
+        b.setNumberLab(50);
+        b.getNumberLab();
+
+        System.out.print(b.getNumberLab());
+
+    }
+
+}
